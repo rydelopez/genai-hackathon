d25a7ca976be8c3ed2154c309548516ccecc2b1b
Ryan De Lopez <delopezryan@gmail.com>
2024-02-17 03:32:05 -0500

    Added APIs

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from './nav';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full bg-gray-50">
+      <body className={inter.className}>
+        <Suspense>
+          <Nav />
+        </Suspense>
+        {children}
+      </body>
     </html>
   );
 }
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,22 @@
 export default async function Home() {
+  const conversation_id = "123";
 
+  const { avg_response_length, unique_length, response_time } = await fetch(
+    `http://localhost:3500/stats/conversation/${conversation_id}`
+  ).then((res) => res.json());
 
   return (
-    <>
+    <div className="p-4 md:p-10 mx-auto max-w-7xl">
       <h1>Hello</h1>
-    </>
+      <p>
+        Average response length: {avg_response_length} characters
+      </p>
+      <p>
+        Unique response length: {unique_length} characters
+      </p>
+      <p>
+        Response time: {response_time} seconds
+      </p>
+    </div>
   );
 }
