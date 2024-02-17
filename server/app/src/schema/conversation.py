f43052073950c3b6d6dd55dbeb508444a002f99a
Shiva Menta <shiva.123.menta@gmail.com>
2024-02-17 07:27:04 -0500

    Modify Chat Model

--- server/app/src/schema/conversation.py
+++ server/app/src/schema/conversation.py
@@ -8,16 +8,8 @@ app = FastAPI()
 
 
 class Message(BaseModel):
-    conversation_id: str | None = "1"
-    project_id: str
-    name: str | None = None
-    message: str | None = None
-    time_stamp: datetime | None = None
-
+    type: str
+    contents: str
 
 class ChatSession(BaseModel):
     message_list: list[Message] = []
-    session_id: uuid.UUID
-    user_id: uuid.UUID
-    project_id: uuid.UUID
-    last_time_used: datetime | None = None
